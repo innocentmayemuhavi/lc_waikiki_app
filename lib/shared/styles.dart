e16b315535154f2b41e46c5038ceb98ba3edd41c
innocentmayemuhavi <97088906+innocentmayemuhavi@users.noreply.github.com>
2023-06-27 18:13:46 +0300

    ./

--- lib/shared/styles.dart
+++ lib/shared/styles.dart
@@ -8,3 +8,17 @@ const productnamestyle = TextStyle(
   fontWeight: FontWeight.w600,
   fontSize: 16,
 );
+const headerStyle = TextStyle(fontSize: 18, fontWeight: FontWeight.w700);
+
+const inputDecorationstyle = InputDecoration(
+  // contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+
+  // enabledBorder: OutlineInputBorder(
+  //   borderSide: BorderSide(color: Colors.black, width: 2),
+  // ),
+  // focusedBorder: OutlineInputBorder(
+  //     borderSide: BorderSide(color: Colors.blue, width: 2)));
+
+
+  border: OutlineInputBorder(),
+);
